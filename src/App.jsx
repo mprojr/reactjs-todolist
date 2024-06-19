a0f45ef4463e75139ad2eea61f0d8006c74317dc
mprojr <martinprojcs@gmail.com>
2024-06-18 21:51:22 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import TodoInput from './components/TodoInput'
+import TodoList from './components/TodoList'
+import TodoCard from './components/TodoCard'
+
+function App() {
+
+  const [todos, setTodos] = useState([])
+  const [todoValue, setTodoValue] = useState([])
+
+  function persistData(newList) {
+    localStorage.setItem('todos', JSON.stringify( { todos: newList}))
+  }
+  
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo]
+    persistData(newTodoList)
+    setTodos(newTodoList)
+  }
+
+  function handleDeleteTodo(index){
+    const newTodoList = todos.filter((todo, todoIndex) => {
+      return todoIndex !== index
+    })
+    persistData(newTodoList)
+    setTodos(newTodoList)
+  }
+
+  function handleEditTodo(index){
+    const valueToBeEdited = todos[index]
+    setTodoValue(valueToBeEdited)
+    handleDeleteTodo(index)
+  }
+
+  useEffect(() => {
+    if (!localStorage) {
+      return
+    }
+
+    let localTodos = localStorage.getItem('todos')
+    if (!localTodos) {
+      return
+    }
+
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos)
+  }, [])
+
+  return (
+    <>
+      <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos}/>
+      <TodoList handleEditTodo={handleEditTodo} handleDeleteTodo={handleDeleteTodo} todos={todos}/>
+      <TodoCard />
+    </>
+  )
+}
+
+export default App
